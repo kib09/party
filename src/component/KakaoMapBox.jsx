6a131870a833b950c819db37054de71b76c1df88
kib09 <dslqoehf@gmail.com>
2025-05-28 14:51:47 +0900

    Update KakaoMapBox.jsx

--- src/component/KakaoMapBox.jsx
+++ src/component/KakaoMapBox.jsx
@@ -4,7 +4,7 @@ function KakaoMapBox() {
   return (
     <Map
       center={{ lat: 36.3581, lng: 128.695 }}
-      style={{ width: "100%", height: "300px" }}
+      className="w-full h-[300px]"
       level={3}
     >
       <MapMarker position={{ lat: 36.3581, lng: 128.6954 }}>
