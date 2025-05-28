38a467dbdba00e596861d241d6cb2c803e7b3e2e
kib09 <dslqoehf@gmail.com>
2025-05-28 13:57:28 +0900

    새로고침 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import FirstPage from "./pages/FirstPage";
 import Mainpage from "./pages/MainPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<FirstPage />} />
-      <Route path="/main" element={<Mainpage />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FirstPage />} />
+        <Route path="/main" element={<Mainpage />} />
+      </Routes>
+    </Router>
   );
 }
 
